b1aa905db6622f70817b81a6117598d687c69009
Jonas <jonasrj97@gmail.com>
2020-09-10 10:55:30 +0200

    new updates

--- Projekt/script.js
+++ Projekt/script.js
@@ -2,9 +2,10 @@ var todoInput = document.querySelector(".todo-input");
 var todoButton = document.querySelector(".todo-button");
 var todoList = document.querySelector(".todo-list");
 
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener('click', createElement);
+todoList.addEventListener('click', todoCheck);
 
-function addTodo (event) {
+function createElement (event) {
     event.preventDefault();
 
     // Opretter et div element
@@ -24,14 +25,38 @@ function addTodo (event) {
     var complteButton = document.createElement('button');
     complteButton.innerHTML = '<i class="fas fa-check"></i>';
     complteButton.classList.add('btn-complete');
+    complteButton.addEventListener('click', function(){
+        //todoLi.style.textDecoration = "line-through";
+    })
     todoDiv.appendChild(complteButton);
     
     // Slet knap
     var deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
     deleteButton.classList.add('btn-delete');
+    deleteButton.addEventListener('click', function(e){
+        // delete code here
+        // var list = document.querySelector(".todo-list");
+        
+        // list.removeChild(list.childNodes[0]);  
+    })
     todoDiv.appendChild(deleteButton);
 
     // Smider alt det overstående ind i vores todoList, som allerede er i vores index.html
     todoList.appendChild(todoDiv);
+}
+
+function todoCheck(e) {
+    // Finder det item vi klikker på
+    var item = e.target;
+    console.log(e.target);
+    // Laver et check på hvad den skal gøre, med det item.
+    if (item.classList[0] === 'btn-delete') {
+        var parentElement = item.parentElement;
+        parentElement.remove();
+    } else if (item.classList[0] === 'btn-complete'){
+        var parentElement = item.parentElement;
+        parentElement.classList.toggle('completed');
+        console.log(parentElement);
+    }
 }
